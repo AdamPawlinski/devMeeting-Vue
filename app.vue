f27c2d38c53a107df606679b31c94b6278ec5105
Adam Pawliński <adam.pawlinski@gmail.com>
2018-03-10 12:16:41 +0100

    init project

---
+++ app.vue
@@ -0,0 +1,17 @@
+<template>
+  <h1>Fruit list</h1>
+  <ul>
+    <li v-for="item in list">
+      {{item.message}}
+    </li>
+  </ul>
+  <p v-if="!list.length"> No Fruits on the list</p>
+</template>
+<script>
+
+</script>
+<style>
+  #app {
+
+  }
+</style>
